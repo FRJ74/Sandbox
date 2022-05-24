486bdd58f01ba6fb717e94b4ea437b51282f9ee9
FRJ74 <frankr74@gmail.com>
2022-05-24 16:31:02 -0500

    Added 3 Q's and answers

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Setting up variables */
 const restartBtn = document.getElementById("restart");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
@@ -7,3 +8,31 @@ const falseBtn = document.getElementById("False");
 const userScore = document.getElementById("user-score");
 const totalScore = document.getElementById("total-score");
 const questionText = document.getElementById("question-text")
+
+/* Multidemtional arrays to prompts questions and track correct answeres*/
+let currentQuestion = 0;
+let score = 0;
+ 
+let questions = [
+   {
+       question: "What is the hardest natural substance on Earth?",
+       answers: [
+           {option: "Diamond", answer: true},
+           {option: "I don’t know.", answer: false},
+       ]
+   },
+   {
+       question: "Which is the main gas that makes up the Earth's atmosphere?",
+       answers: [
+           {option: "I don’t know", answer: false},
+           {option: "Nitrogen", answer: true},
+       ]
+   },
+   {
+       question: "Roughly how long does it take for the sun's light to reach Earth - 8 minutes, 8 hours or 8 days?",
+       answers: [
+           {option: "8 hours" , “8 days”, answer: false},
+           {option: "8 minutes", answer: true},
+       ]
+   }
+]
